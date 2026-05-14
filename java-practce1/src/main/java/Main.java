9bb1319e2fa8e21e222f473558ff499998f7b931
smile308 <tkingstarh@naver.com>
2026-05-14 16:01:26 +0900

    study

---
+++ java-practce1/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "태현";
+        int age = 27;
+        double height = 167.1;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
