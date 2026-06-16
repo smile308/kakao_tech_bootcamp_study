4ded11e699c80b86656728a027cafc4af390fc12
smile308 <tkingstarh@naver.com>
2026-06-16 17:55:35 +0900

    persist flush remove

--- jpa-practice1/src/test/java/kr/adapterz/jpa_practice/entity/UserTest.java
+++ jpa-practice1/src/test/java/kr/adapterz/jpa_practice/entity/UserTest.java
@@ -35,4 +35,38 @@ class UserTest {
             entityManager.persist(user);
         }
     }
+
+    @Test
+    @Rollback(false)
+    void flushTest() {
+        User user = new User("tester@adapterz.kr", "123aS!", "Adapterz");
+
+        System.out.println("=== Flush (아무것도 없음) ===");
+        entityManager.flush(); // 아무 것도 없음 (정상)
+        System.out.println("==============");
+
+        System.out.println("=== Persist ===");
+        entityManager.persist(user);     // 영속화 (아직 INSERT 미발행)
+        System.out.println("==============");
+
+        System.out.println("=== Flush (INSERT 발생) ===");
+        entityManager.flush();           // 여기서 INSERT 발생
+        System.out.println("==============");
+    }
+
+    @Test
+    @Rollback(false)
+    void removeTest() {
+        User user = new User("delete@adapterz.kr", "123aS!", "DeleteUser");
+        entityManager.persist(user);
+
+        entityManager.flush(); // INSERT 실행
+        System.out.println("=== INSERT 쿼리 실행됨 ===");
+
+        entityManager.remove(user);
+        System.out.println("=== remove 호출 (아직 DELETE 쿼리 안 나감) ===");
+
+        entityManager.flush();
+        System.out.println("=== DELETE 쿼리 실행됨 ===");
+    }
 }
