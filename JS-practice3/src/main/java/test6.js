fa4be9b63f86c659c42601fbbcd277f53c3b0aec
smile308 <tkingstarh@naver.com>
2026-06-24 15:20:45 +0900

    비동기 처리 미니 퀘스트

--- JS-practice3/src/main/java/test6.js
+++ JS-practice3/src/main/java/test6.js
@@ -2,10 +2,17 @@
 
 const myFirstPromise = new Promise((resolve, reject) => {
     // 여기에 코드를 작성하세요.
-
+    resolve("Hello, Promise!");
 });
 
 // 아래 코드는 수정하지 마세요.
 myFirstPromise.then(message => {
     console.log(message);
 });
+
+
+async function  waitForMessage (){
+    setTimeout(()=>console.log("Hello,Async/Await!"),1000);
+}
+
+waitForMessage();
