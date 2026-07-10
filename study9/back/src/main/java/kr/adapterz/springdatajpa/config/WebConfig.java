1d0a33d64e32da44120cc7ca9427bafc28952d59
smile308 <70571820+smile308@users.noreply.github.com>
2026-07-10 17:46:16 +0900

    리액트화

--- study9/back/src/main/java/kr/adapterz/springdatajpa/config/WebConfig.java
+++ study9/back/src/main/java/kr/adapterz/springdatajpa/config/WebConfig.java
@@ -16,6 +16,9 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5500",
+                                "http://127.0.0.1:5500",
+
+                                "http://localhost:5500",
                                 "http://127.0.0.1:5500"
                         )
                         .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
--- study9/react/src/main.jsx
+++ study9/react/src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+import "./styles/common.css";
+import "./styles/auth.css";
+import "./styles/posts.css";
+import "./styles/user.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
